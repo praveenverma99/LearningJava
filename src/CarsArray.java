20ba817e09e5d2836dfe44ca964868ccd39e9fe7
Praveen Verma <praveenvermamail@gmail.com>
2022-07-06 19:34:20 +0530

    1darray

---
+++ src/CarsArray.java
@@ -0,0 +1,15 @@
+public class CarsArray {
+    public static void main(String[] args) {
+
+        String[] cars = new String[4] ;
+           cars[0] = "Maruti";
+           cars[1] = "Tata";
+           cars[2] = "Hyundai";
+           cars[3] = "Honda";
+        System.out.println(cars[1]);
+
+        for( int i=0; i< cars.length; i++){
+            System.out.println(cars[i]);
+        }
+    }
+}
