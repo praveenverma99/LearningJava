cf27e84db17f67d5115ece847a64710787c2e317
Praveen Verma <praveenvermamail@gmail.com>
2022-07-07 07:08:09 +0530

    string

---
+++ src/Cars2D.java
@@ -0,0 +1,18 @@
+public class Cars2D {
+    public static void main(String[] args) {
+
+
+
+          String[][]  cars = { {"m" , "h" , "t"} ,
+                              {"h" , "b" , "i"} };
+
+        //System.out.println(cars[1][1]);
+
+        for (int i=0;i<cars.length; i++) {
+             System.out.println();
+            for (int j= 0; j<cars[i].length; j++) {
+                System.out.print(cars[i][j]+ " ");
+            }
+        }
+    }
+}
