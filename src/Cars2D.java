a6f79a3cf8576df1eb0282f2ab0f9aba9302c7a5
Praveen Verma <praveenvermamail@gmail.com>
2022-07-08 09:49:11 +0530

    Methods

--- src/Cars2D.java
+++ src/Cars2D.java
@@ -2,16 +2,14 @@ public class Cars2D {
     public static void main(String[] args) {
 
 
+        String[][] cars = {{"m", "h", "t" },
+                {"h", "b", "i" }};
 
-          String[][]  cars = { {"m" , "h" , "t"} ,
-                              {"h" , "b" , "i"} };
-
-        //System.out.println(cars[1][1]);
-
-        for (int i=0;i<cars.length; i++) {
-             System.out.println();
-            for (int j= 0; j<cars[i].length; j++) {
-                System.out.print(cars[i][j]+ " ");
+       //  System.out.println(cars[1][1]);
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print("  " + cars[i][j] + " ");
             }
         }
     }
---
+++ src/Stringmethods.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class Stringmethods {
+    public static void main(String[] args) {
+
+         String name = "Praveen";
+
+        // boolean result =  name.equalsIgnoreCase("Prav");
+         // int result = name.length();
+         //   char result = name.charAt(1);
+          //  int result = name.indexOf("n");
+           // boolean result = name.isEmpty();
+        //String result = name.toLowerCase();
+        //String result = name.toUpperCase();
+        //String result = name.replace("e", "a");
+
+        System.out.println(result);
+    }
+}
