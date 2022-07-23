e91b30ebccdc9469f511bab5e81d91d2e6ba206f
Praveen Verma <praveenvermamail@gmail.com>
2022-07-23 19:36:46 +0530

    Merge branch 'main' of https://github.com/praveenverma99/LearningJava

--- src/com/praveen/polymorphism/Main.java
+++ src/com/praveen/polymorphism/Main.java
@@ -1,15 +1,13 @@
 package com.praveen.polymorphism;
 
 public class Main {
-
     static Car Maruti = new Car();
     static Boat Hero = new Boat();
-
-    Vehicle[] Racers = {Maruti, Hero};
+    static Bicyle Monster = new Bicyle();
 
     public static void main(String[] args) {
 
-        Vehicle[] Racers = {Maruti, Hero};
+        Vehicle[] Racers = {Maruti, Hero , Monster};
         for (Vehicle x : Racers) {
             x.go();
         }
